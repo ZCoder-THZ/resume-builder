f8c488ba6557c075e4dae3efa01da79c0d0f29af
thanhtikezaw <thanhtikezaw919@gmail.com>
2024-11-06 16:19:39 +0700

    feats:added Social Links

--- front/src/home/SocialLinks.tsx
+++ front/src/home/SocialLinks.tsx
@@ -1,7 +1,73 @@
+import { TextField } from '@mui/material';
 import React from 'react';
-import { Controller } from 'react-hook-form';
+import { useFormContext, Controller } from 'react-hook-form';
 function SocialLinks() {
-  return <div>SocialLinks</div>;
+  const { control } = useFormContext();
+  return (
+    <>
+      <Controller
+        control={control}
+        name="portFolio_website"
+        render={({ field }) => (
+          <TextField
+            fullWidth={true}
+            sx={{ mb: 2 }}
+            label="Portfolio"
+            {...field}
+          />
+        )}
+      />
+      <Controller
+        control={control}
+        name="github"
+        render={({ field }) => (
+          <TextField
+            label="Github"
+            sx={{ mb: 2 }}
+            {...field}
+            fullWidth={true}
+            {...field}
+          />
+        )}
+      />
+      <Controller
+        control={control}
+        name="linkedin"
+        render={({ field }) => (
+          <TextField
+            fullWidth={true}
+            sx={{ mb: 2 }}
+            label="Linkedin"
+            {...field}
+          />
+        )}
+      />
+      <Controller
+        control={control}
+        name="facebook"
+        render={({ field }) => (
+          <TextField
+            fullWidth={true}
+            sx={{ mb: 2 }}
+            label="Facebook"
+            {...field}
+          />
+        )}
+      />
+      <Controller
+        control={control}
+        name="twitter"
+        render={({ field }) => (
+          <TextField
+            fullWidth={true}
+            sx={{ mb: 2 }}
+            label="Twitter"
+            {...field}
+          />
+        )}
+      />
+    </>
+  );
 }
 
 export default SocialLinks;
